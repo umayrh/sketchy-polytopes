e26f5d6e665b5674f8ff1fb945ce1ae5eebc55ed
Umayr <umayrh@gmail.com>
2018-07-07 21:19:48 -0700

    Test for translating node property

--- python/evolvingdag/src/random_dag.py
+++ python/evolvingdag/src/random_dag.py
@@ -1,88 +1,10 @@
 from networkx import DiGraph
 from networkx.algorithms.dag import descendants
-import numpy.random  # NOQA
 
 
 class RandomDag:
-    """Represents an instance of a RandomGraphModel.
-
-    The model uses a random sample from each property with a random model.
-    """
-    def __init__(self, dag_model):
-        self.model = dag_model
-
-    @staticmethod
-    def _update_property(data, property_name, default_weight):
-        """ Update a node or edge property with given name.
-
-        Args:
-            data (dict): map of node/edge properties
-            property_name: name of a property (i.e. a key for 'data')
-            default_weight (float): default value of the property
-        """
-        if property_name in data:
-            property_val = data[property_name]
-            if property_val is not None and len(property_val) > 1:
-                func_args = property_val[1:]
-                return property_val[0](*func_args)
-        return default_weight
-
-    def sample_node_property(self, node_property,
-                             weight_key='w',
-                             default_weight=0):
-        """ Samples nodes with given property, and sets weight_key with the
-        sampled, or default, value.
-
-        Args:
-            node_property (str): name of node property to sample
-            weight_key (str): name of property that holds sampled value
-            default_weight (float): default value of the weight_key property
-        """
-        if node_property is None or node_property is "":
-            raise Exception("Invalid property")
-
-        for (node, data) in list(self.model.dag.nodes(data=True)):
-            property_weight = RandomDag._update_property(
-                data, node_property, default_weight)
-            self.model.dag.nodes[node][weight_key] = property_weight
-
-    def sample_edge_property(self, edge_property,
-                             weight_key='w',
-                             default_weight=0):
-        """ Samples edges with given property, and sets weight_key with the
-        sampled, or default, value.
-
-        Args:
-            edge_property (str): name of edge property to sample
-            weight_key (str): name of property that holds sampled value
-            default_weight (float): default value of the weight_key property
-        """
-        if edge_property is None or edge_property is "":
-            raise Exception("Invalid property")
-
-        for (s, t, data) in list(self.model.dag.edges(data=True, default={})):
-            property_weight = RandomDag._update_property(
-                data, edge_property, default_weight)
-            self.model.dag[s][t][weight_key] = property_weight
-
-    def node_property_to_edge(self):
-        # TODO
-        print("no implemented")
-
-    def longest_path(self):
-        # TODO
-        print("no implemented")
-
-    def second_longest_path(self):
-        # TODO
-        print("no implemented")
-
-
-class RandomDagModel:
-    """Utilities for creating random directed acyclic graphs.
-
-    This class represents a simple DAG with properties
-    that might be random models e.g. a 'runtime' property for each node
+    """This class represents a simple DAG with properties
+    that might be random variable e.g. a 'runtime' property for each node
     with value from the uniform distribution, U(30, 50).
 
     Each node may be associated with key-value pairs of properties. The key
@@ -125,6 +47,94 @@ class RandomDagModel:
             self.dag.add_edge(from_node.name, to_node.name)
         else:
             self.dag.add_edge(from_node.name, to_node.name, **properties)
+
+    @staticmethod
+    def __update_property(data, property_name, default_weight):
+        """ Updates a node or edge property with given name.
+
+        Args:
+            data (dict): map of node/edge properties
+            property_name: name of a property (i.e. a key for 'data')
+            default_weight (float): default value of the property
+        """
+        if property_name in data:
+            property_val = data[property_name]
+            if property_val is not None and len(property_val) > 1:
+                func_args = property_val[1:]
+                return property_val[0](*func_args)
+        return default_weight
+
+    def sample_node_property(self, node_property,
+                             weight_key='w',
+                             default_weight=0):
+        """ Samples nodes with given property, and sets weight_key with the
+        sampled, or default, value.
+
+        Args:
+            node_property (str): name of node property to sample
+            weight_key (str): name of property that holds sampled value
+            default_weight (float): default value of the weight_key property
+        """
+        if node_property is None or node_property is "":
+            raise Exception("Invalid property")
+
+        for (node, data) in list(self.dag.nodes(data=True)):
+            property_weight = RandomDag.__update_property(
+                data, node_property, default_weight)
+            self.dag.nodes[node][weight_key] = property_weight
+
+    def sample_edge_property(self, edge_property,
+                             weight_key='w',
+                             default_weight=0):
+        """ Samples edges with given property, and sets weight_key with the
+        sampled, or default, value.
+
+        Args:
+            edge_property (str): name of edge property to sample
+            weight_key (str): name of property that holds sampled value
+            default_weight (float): default value of the weight_key property
+        """
+        if edge_property is None or edge_property is "":
+            raise Exception("Invalid property")
+
+        for (s, t, data) in list(self.dag.edges(data=True, default={})):
+            property_weight = RandomDag.__update_property(
+                data, edge_property, default_weight)
+            self.dag[s][t][weight_key] = property_weight
+
+    def node_property_to_edge(self, start_node_name="start",
+                              node_property='w',
+                              edge_property='w'):
+        """ Translates a node property to an edge property by modifying the
+        underlying DAG to (1) add a "start" node, and to (2) add an
+        edge weight, which is the weight of its successor node.
+
+        Args:
+            start_node_name (str): name of the node to add that will be
+               topologically precedent to all others
+            node_property (str): name of node property to get
+            edge_property (str): name of edge property to set
+        """
+        if len(self.dag) == 0:
+            return
+        # add an edge to existing start nodes
+        start = Node(start_node_name)
+        zero_indegree = [v for v, d in self.dag.in_degree() if d == 0]
+        for node in zero_indegree:
+            self.dag.add_edge(start.name, node)
+        for (s, t) in list(self.dag.edges(data=False)):
+            self.dag[s][t][edge_property] = self.dag.nodes[t][node_property]
+
+    def second_longest_path(self, weight='w', default_weight=0):
+        """
+
+        Args:
+            weight (str): edge data key to use for weight
+            default_weight (int): weight of edges that do
+               not have a weight attribute
+        """
+        # TODO
+        print(["not implemented", weight, default_weight])
 
 
 class Node:
--- python/evolvingdag/test/test_random_dag.py
+++ python/evolvingdag/test/test_random_dag.py
@@ -1,10 +1,23 @@
 import unittest
 import pytest
-from random_dag import Node, RandomDagModel, RandomDag
+from random_dag import Node, RandomDag
 from numpy.random import uniform
 
 
 class Test(unittest.TestCase):
+    @staticmethod
+    def make_model():
+        a = Node("a", {"x": [uniform, 10, 20]})
+        b = Node("b", {"y": [uniform, 20, 30]})
+        c = Node("c", {"z": [uniform, 30, 40]})
+        model = RandomDag()
+        model.add_node(a)
+        model.add_node(b)
+        model.add_node(c)
+        model.add_edge(a, b, {"w": 2})
+        model.add_edge(b, c, {"r": [uniform, 30, 40]})
+        return model
+
     def test_node(self):
         """Tests if a node with name and properties can be created correctly"""
         a = Node("a")
@@ -20,68 +33,62 @@ class Test(unittest.TestCase):
         c_expected = {"time": 1, "class": "xyz", c.NODE_LABEL_KEY: set("ctx")}
         self.assertEqual(c_expected, c.properties)
 
-    def test_random_dag_model(self):
+    def test_random_dag_add(self):
         """Tests if a RandomDagModel can be created correctly"""
-        a = Node("a")
-        b = Node("b")
-        dag = RandomDagModel()
-        dag.add_node(a)
-        dag.add_node(b)
-        dag.add_edge(a, b, {"w": 2})
+        model = Test.make_model()
 
-        self.assertTrue(a.name in dag.dag)
-        self.assertTrue(b.name in dag.dag)
-        self.assertFalse("c" in dag.dag)
-        self.assertFalse(dag.dag.succ[b.name])
-        self.assertFalse(dag.dag.pred[a.name])
+        self.assertTrue("a" in model.dag)
+        self.assertTrue("b" in model.dag)
+        self.assertFalse("e" in model.dag)
 
-        self.assertTrue(dag.dag.has_edge("a", "b"))
-        self.assertTrue(2, dag.dag["a"]["b"]["w"])
-        self.assertFalse("d" in dag.dag["a"]["b"])
+        self.assertTrue(model.dag.has_edge("a", "b"))
+        self.assertTrue(2, model.dag["a"]["b"]["w"])
+        self.assertFalse("d" in model.dag["a"]["b"])
 
-        pytest.raises(Exception, dag.add_edge, b, a)
-        dag.add_edge(b, a, {"w": 2}, False)
-        self.assertTrue(dag.dag.has_edge("b", "a"))
+        pytest.raises(Exception, model.add_edge, "b", "a")
+        model.add_edge(Node("b"), Node("a"), {"w": 2}, False)
+        self.assertTrue(model.dag.has_edge("b", "a"))
 
-    def test_random_dag(self):
+    def test_random_dag_sample(self):
         """Tests if a RandomDag can be created correctly"""
-        a = Node("a", {"x": [uniform, 10, 20]})
-        b = Node("b", {"y": [uniform, 20, 30]})
-        c = Node("c", {"z": [uniform, 30, 40]})
-        model = RandomDagModel()
-        model.add_node(a)
-        model.add_node(b)
-        model.add_node(c)
-        model.add_edge(a, b, {"w": 2})
-        model.add_edge(b, c, {"r": [uniform, 30, 40]})
-        dag = RandomDag(model)
+        model = Test.make_model()
 
-        dag.sample_edge_property("q")
-        self.assertTrue(dag.model.dag.has_edge("a", "b"))
-        self.assertTrue(dag.model.dag.has_edge("b", "c"))
-        self.assertEqual(0, dag.model.dag["a"]["b"]["w"])
-        self.assertEqual(0, dag.model.dag["b"]["c"]["w"])
+        model.sample_edge_property("q")
+        self.assertTrue(model.dag.has_edge("a", "b"))
+        self.assertTrue(model.dag.has_edge("b", "c"))
+        self.assertEqual(0, model.dag["a"]["b"]["w"])
+        self.assertEqual(0, model.dag["b"]["c"]["w"])
 
-        dag.sample_edge_property("r")
-        self.assertTrue(dag.model.dag.has_edge("a", "b"))
-        self.assertTrue(dag.model.dag.has_edge("b", "c"))
-        self.assertEqual(0, dag.model.dag["a"]["b"]["w"])
-        self.assertTrue(30 <= dag.model.dag["b"]["c"]["w"] < 40)
+        model.sample_edge_property("r")
+        self.assertTrue(model.dag.has_edge("a", "b"))
+        self.assertTrue(model.dag.has_edge("b", "c"))
+        self.assertEqual(0, model.dag["a"]["b"]["w"])
+        self.assertTrue(30 <= model.dag["b"]["c"]["w"] < 40)
 
-        dag.sample_node_property("x")
-        self.assertTrue(10 <= dag.model.dag.nodes["a"]["w"] < 20)
-        self.assertTrue(dag.model.dag.nodes["b"]["w"] == 0)
-        self.assertTrue(dag.model.dag.nodes["c"]["w"] == 0)
+        model.sample_node_property("x")
+        self.assertTrue(10 <= model.dag.nodes["a"]["w"] < 20)
+        self.assertTrue(model.dag.nodes["b"]["w"] == 0)
+        self.assertTrue(model.dag.nodes["c"]["w"] == 0)
 
-        dag.sample_node_property("y")
-        self.assertTrue(dag.model.dag.nodes["a"]["w"] == 0)
-        self.assertTrue(20 <= dag.model.dag.nodes["b"]["w"] < 30)
-        self.assertTrue(dag.model.dag.nodes["c"]["w"] == 0)
+        model.sample_node_property("y")
+        self.assertTrue(model.dag.nodes["a"]["w"] == 0)
+        self.assertTrue(20 <= model.dag.nodes["b"]["w"] < 30)
+        self.assertTrue(model.dag.nodes["c"]["w"] == 0)
 
-        dag.sample_node_property("z")
-        self.assertTrue(dag.model.dag.nodes["a"]["w"] == 0)
-        self.assertTrue(dag.model.dag.nodes["b"]["w"] == 0)
-        self.assertTrue(30 <= dag.model.dag.nodes["c"]["w"] < 40)
+        model.sample_node_property("z")
+        self.assertTrue(model.dag.nodes["a"]["w"] == 0)
+        self.assertTrue(model.dag.nodes["b"]["w"] == 0)
+        self.assertTrue(30 <= model.dag.nodes["c"]["w"] < 40)
+
+    def test_random_dag_property(self):
+        """Tests if a RandomDag can be created correctly"""
+        model = Test.make_model()
+        model.sample_node_property("x")
+        model.node_property_to_edge(start_node_name="s")
+        self.assertTrue("s" in model.dag)
+        self.assertTrue(10 <= model.dag["s"]["a"]["w"] < 20)
+        self.assertTrue(model.dag["a"]["b"]["w"] == 0)
+        self.assertTrue(model.dag["b"]["c"]["w"] == 0)
 
 if __name__ == "__main__":
     unittest.main()
