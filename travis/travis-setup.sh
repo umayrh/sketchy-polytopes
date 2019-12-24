63fa9e1f2cd70612705705470ff1f6c87763d833
Umayr Hassan <umayrh@gmail.com>
2019-12-24 01:24:06 -0800

    R:rUtils - add debug code

--- travis/travis-setup.sh
+++ travis/travis-setup.sh
@@ -83,7 +83,7 @@ setupR() {
     sudo add-apt-repository -y "ppa:marutter/c2d4u3.5"
 
     retry sudo apt-get -qq update
-    retry sudo apt-get -qq install -y --no-install-recommends r-base-dev r-recommended qpdf
+    retry sudo apt-get -qq install -y --no-install-recommends r-base-dev r-recommended qpdf libssh2-1-dev libgit2-dev
 
     sudo chmod 2777 /usr/local/lib/R /usr/local/lib/R/site-library
 }
